330bcc2042c943e6e9a96bfaef23039ed0f0d4dd
Szymon Świtała <szymon.switalla@gmail.com>
2023-04-01 15:57:33 +0200

    added header to stories page

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -12,36 +12,36 @@ import Grid from '../../atoms/Grid';
 import StoryCard from '../../molecules/StoryCard';
 import FeatureBox from '../../molecules/FeatureBox';
 
-const Home = () => {
-  return (
-    <Wrapper className={styles.wrapper}>
-      <main>
-        <section>
-          {infoBoxes.map((data, i) => (
-            <InfoBox {...data} theme={i === 0 ? 'dark' : 'light'} variant={i === 1 ? 'left' : 'right'} key={crypto.randomUUID()} />
+const Home = () => (
+  <Wrapper className={styles.wrapper}>
+    <main>
+      <section>
+        {infoBoxes.map((data, i) => (
+          <InfoBox {...data} theme={i === 0 ? 'dark' : 'light'} variant={i === 1 ? 'left' : 'right'} key={crypto.randomUUID()} />
+        ))}
+      </section>
+
+      <section>
+        <Grid container>
+          {storyCards.slice(0, 4).map(data => (
+            <Grid item xs={12} sm={6} lg={3} key={crypto.randomUUID()}>
+              <StoryCard {...data} />
+            </Grid>
           ))}
-        </section>
-        <section>
-          <Grid container>
-            {storyCards.slice(0, 4).map(data => (
-              <Grid item xs={12} sm={6} lg={3} key={crypto.randomUUID()}>
-                <StoryCard {...data} />
-              </Grid>
-            ))}
-          </Grid>
-        </section>
-        <section>
-          <Grid container spacing={useWindowSize() >= 992 ? 'lg' : undefined} className={styles.features}>
-            {features.slice(0, 3).map(data => (
-              <Grid item xs={12} lg={4} key={crypto.randomUUID()}>
-                <FeatureBox {...data} />
-              </Grid>
-            ))}
-          </Grid>
-        </section>
-      </main>
-    </Wrapper>
-  );
-};
+        </Grid>
+      </section>
+
+      <section>
+        <Grid container spacing={useWindowSize() >= 992 ? 'lg' : undefined} className={styles.features}>
+          {features.slice(0, 3).map(data => (
+            <Grid item xs={12} lg={4} key={crypto.randomUUID()}>
+              <FeatureBox {...data} />
+            </Grid>
+          ))}
+        </Grid>
+      </section>
+    </main>
+  </Wrapper>
+);
 
 export default Home;
--- src/components/pages/Stories/index.tsx
+++ src/components/pages/Stories/index.tsx
@@ -1,7 +1,46 @@
 import styles from './Stories.module.scss';
+import { header, storyCards } from '../../../data/stories.json';
+
+import useGetImageSrc from '../../../hooks/useGetImageSrc';
+
+import Wrapper from '../../atoms/Wrapper';
+import Heading from '../../atoms/Heading';
+import Text from '../../atoms/Text';
+import Button from '../../atoms/Button';
+
+const { src, heading, mainHeading, createdAt, createdBy, text, btnText } = header;
 
 const Stories = () => {
-  return <div>Stories</div>;
+  return (
+    <Wrapper>
+      <header className={styles.header}>
+        <div className={styles.header_img}>
+          <div className={styles.shadow} />
+          <img src={useGetImageSrc(src, true)} />
+        </div>
+
+        <div className={styles.header_content}>
+          <Heading level="4" theme="light">
+            {heading}
+          </Heading>
+          <Heading level="2" theme="light">
+            {mainHeading}
+          </Heading>
+          <p className={styles.created}>
+            <span>{createdAt}</span> {createdBy}
+          </p>
+          <Text theme="light">{text}</Text>
+          <Button theme="light" variant="arrow">
+            {btnText}
+          </Button>
+        </div>
+      </header>
+
+      <main>
+
+      </main>
+    </Wrapper>
+  );
 };
 
 export default Stories;
