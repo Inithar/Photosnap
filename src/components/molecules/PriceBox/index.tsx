a6a4a8ac05f78108433de0fa616cd8c41366d125
Szymon Świtała <szymon.switalla@gmail.com>
2023-04-03 14:58:57 +0200

    added PriceBox component and pricing section to pricing page

---
+++ src/components/molecules/PriceBox/index.tsx
@@ -0,0 +1,55 @@
+import styles from './PriceBox.module.scss';
+
+import Button from '../../atoms/Button';
+import Heading from '../../atoms/Heading';
+import Text from '../../atoms/Text';
+import classNames from 'classnames/bind';
+
+interface PriceBoxProps {
+  theme?: 'dark' | 'light';
+  period: 'month' | 'year';
+  bigger?: boolean;
+  heading: string;
+  text: string;
+  price: {
+    month: string;
+    year: string;
+  };
+}
+
+const PriceBox = ({ heading, text, price, theme = 'light', bigger, period }: PriceBoxProps) => {
+  const secondaryTheme = theme === 'dark' ? 'light' : 'dark';
+
+  const cx = classNames.bind(styles);
+
+  const boxClassNames = cx('box', {
+    dark_box: theme === 'dark',
+    bigger_box: bigger
+  });
+
+  const priceBoxClassNames = cx('price_box', {
+    dark_price_box: theme === 'dark'
+  });
+
+  return (
+    <div className={boxClassNames}>
+      <div>
+        <div className={styles.description}>
+          <Heading level="3" theme={secondaryTheme}>
+            {heading}
+          </Heading>
+          <Text theme={secondaryTheme}>{text}</Text>
+        </div>
+
+        <div className={priceBoxClassNames}>
+          <p className={styles.price}>{price[period]}</p>
+          <p className={styles.period}>per {period}</p>
+        </div>
+      </div>
+
+      <Button theme={secondaryTheme}>Pick plan</Button>
+    </div>
+  );
+};
+
+export default PriceBox;
--- src/components/pages/Pricing/index.tsx
+++ src/components/pages/Pricing/index.tsx
@@ -1,26 +1,45 @@
-import styles from './Pricing.styles.scss';
-import { header } from '../../../data/pricing.json';
+import styles from './Pricing.module.scss';
+import { header, pricingBoxes } from '../../../data/pricing.json';
+
+import { useState } from 'react';
 
 import Wrapper from '../../atoms/Wrapper';
 import InfoBox from '../../organisms/InfoBox';
 import Beta from '../../organisms/Beta';
 import ToggleInput from '../../atoms/ToggleInput';
+import PriceBox from '../../molecules/PriceBox';
 
-const Pricing = () => (
-  <Wrapper>
-    <header>
-      <InfoBox {...header} theme="dark" variant="right" />
-    </header>
-    <main>
-      <section style={{ padding: '2rem' }}>
-        <ToggleInput />
-      </section>
-      <section></section>
-      <section>
-        <Beta />
-      </section>
-    </main>
-  </Wrapper>
-);
+const Pricing = () => {
+  const [period, setPeriod] = useState<'month' | 'year'>('month');
+
+  return (
+    <Wrapper className={styles.wrapper}>
+      <header>
+        <InfoBox {...header} theme="dark" variant="right" />
+      </header>
+      <main>
+        <section className={styles.pricing}>
+          <div className={styles.period}>
+            <p>Monthly</p>
+            <ToggleInput onChange={e => setPeriod(e.target.checked ? 'year' : 'month')} />
+            <p>Yearly</p>
+          </div>
+
+          <div className={styles.pricing_boxes}>
+            {pricingBoxes.map((data, i) => (
+              <PriceBox {...data} period={period} theme={i === 1 ? 'dark' : 'light'} bigger={i === 1} key={crypto.randomUUID()} />
+            ))}
+          </div>
+        </section>
+
+        <section></section>
+
+        <section>
+          <Beta />
+        </section>
+      </main>
+    </Wrapper>
+  );
+};
 
 export default Pricing;
