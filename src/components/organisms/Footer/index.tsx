50f594b4f7764329abdb09adb9f2db52dbdeef82
Szymon Świtała <szymon.switalla@gmail.com>
2023-04-04 12:41:34 +0200

    changed footer layout

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -8,37 +8,39 @@ import Button from '../../atoms/Button';
 import NavLink from '../../atoms/NavLink';
 
 const Footer = () => (
-  <footer className={styles.footer}>
-    <Wrapper>
-      <div className={styles.left_section}>
-        <img src="./assets/shared/desktop/logo-white.svg" alt="Photosnap" />
+  <Wrapper>
+    <footer className={styles.footer}>
+      <div>
+        <div className={styles.left_section}>
+          <img src="./assets/shared/desktop/logo-white.svg" alt="Photosnap" />
 
-        <div className={styles.socials}>
-          {footerIcons.map(({ icon, to }) => (
-            <Link to={to} target="_blank" key={crypto.randomUUID()}>
-              <img src={icon} alt="" />
-            </Link>
-          ))}
+          <div className={styles.socials}>
+            {footerIcons.map(({ icon, to }) => (
+              <Link to={to} target="_blank" key={crypto.randomUUID()}>
+                <img src={icon} alt="" />
+              </Link>
+            ))}
+          </div>
+
+          <nav className={styles.nav}>
+            {footerLinks.map(({ to, text }) => (
+              <NavLink to={to} key={crypto.randomUUID()}>
+                {text}
+              </NavLink>
+            ))}
+          </nav>
         </div>
 
-        <nav className={styles.nav}>
-          {footerLinks.map(({ to, text }) => (
-            <NavLink to={to} key={crypto.randomUUID()}>
-              {text}
-            </NavLink>
-          ))}
-        </nav>
-      </div>
+        <div className={styles.right_section}>
+          <Button variant="arrow" theme="light">
+            Get an invite
+          </Button>
 
-      <div className={styles.right_section}>
-        <Button variant="arrow" theme="light">
-          Get an invite
-        </Button>
-
-        <p>Copyright {new Date().getFullYear()}. All Rights Reserved</p>
+          <p>Copyright {new Date().getFullYear()}. All Rights Reserved</p>
+        </div>
       </div>
-    </Wrapper>
-  </footer>
+    </footer>
+  </Wrapper>
 );
 
 export default Footer;
